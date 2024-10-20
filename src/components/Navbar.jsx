943655519db6e7895d65a4d4c96c30e1f09d3723
Izhan <zikrizhan@gmail.com>
2024-10-20 14:55:10 +0800

    fix connect wallet from browser

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,18 @@ function Navbar() {
   const etherBalance = useEtherBalance(account);
 
   const connectWallet = () => {
-    activateBrowserWallet();
+    if (window.ethereum) {
+      // Request account access
+      activateBrowserWallet();
+    } else {
+      // Redirect to MetaMask app if not installed
+      window.location.href =
+        "https://metamask.app.link/dapp/based-hackathon.vercel.app"; // Updated with your app URL
+    }
   };
 
   return (
     <nav className="bg-primary p-4 flex justify-between items-center">
-      {" "}
-      {/* Changed to bg-primary */}
       <h2 className="text-white text-2xl font-bold">Quadratic Funding</h2>
       <div className="wallet-info flex items-center">
         {!account ? (
